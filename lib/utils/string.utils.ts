61cf18f96c8c08fd8fcbb041fc1c99f84aaa8378
Max Friedmann <max@smallstack.com>
2024-11-15 06:51:17 +0100

    Fix exported types

--- lib/utils/string.utils.ts
+++ lib/utils/string.utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Capitalize the first letter of a string
+ *
+ * @param str The input string
+ * @returns The string with the first letter capitalized
+ */
 export function upperCaseFirst(str: string): string {
   if (!str) return str;
   return str.charAt(0).toUpperCase() + str.slice(1);
